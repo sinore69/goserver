01dc92f794c58089fbdb26db415cd21b9c5b1822
saptarshi dutta <saptarshidutta599@gmail.com>
2024-03-25 20:17:54 +0530

    air

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello world")
+	})
 
-    app.Listen("127.0.0.1:3000")
+	fmt.Println("server is running on port 3000")
+	app.Listen("127.0.0.1:3000")
 }
