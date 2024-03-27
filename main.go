b38cddbdf912930b964ef9e013576383b34cac13
saptarshi dutta <saptarshidutta599@gmail.com>
2024-03-27 20:09:20 +0530

    http request

--- main.go
+++ main.go
@@ -6,13 +6,29 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type info struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world")
+		return c.SendString("hello world!!")
 	})
+	app.Post("/api", post)
 
-	fmt.Println("server is running on port 3000")
-	app.Listen("127.0.0.1:3000")
+	fmt.Println("server is running on port 5000")
+	app.Listen("127.0.0.1:5000")
+}
+
+func post(c *fiber.Ctx) error {
+	data:=new(info)
+	if err := c.BodyParser(data); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+		  "errors": err.Error(),
+		})
+	  }
+	fmt.Println(data)
+	return c.Send(c.Body())
 }
