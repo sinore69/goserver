fcd71ca65bfc077c73f672d3f560b5bb68988600
saptarshi dutta <saptarshidutta599@gmail.com>
2024-04-04 15:45:50 +0530

    params

--- main.go
+++ main.go
@@ -2,36 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"goserver/cryptography"
 	"github.com/gofiber/fiber/v2"
+	"goserver/cryptography"
+	"log"
 )
 
 type info struct {
-	Name string `json:"name"`
-	Shift int `json:"shift"`
+	Name  string `json:"name"`
+	Shift int    `json:"shift"`
 }
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world!!")
-	})
+	app.Get("/:name", name)
 	app.Post("/api", post)
 
 	fmt.Println("server is running on port 5000")
 	app.Listen("127.0.0.1:5000")
 }
+func name(c *fiber.Ctx) error {
+	return c.SendString(c.Params("name"))
+}
 
 func post(c *fiber.Ctx) error {
-	data:=new(info)
+	data := new(info)
 	if err := c.BodyParser(data); err != nil {
 		log.Fatal(err)
-	  }
-	encryptedText:=cryptography.Encrypt(data.Name,data.Shift)
+	}
+	encryptedText := cryptography.Encrypt(data.Name, data.Shift)
 	fmt.Println(encryptedText)
-	decryptedText:=cryptography.Decrypt(encryptedText,data.Shift)
+	decryptedText := cryptography.Decrypt(encryptedText, data.Shift)
 	fmt.Println(decryptedText)
 	return c.Send(c.Body())
 }
